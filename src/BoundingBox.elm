b17b14993d7de394cc159f531e27030ccf294948
hucal <hrivera@nmt.edu>
2015-05-21 23:44:09 -0600

    Factored out Bounding Box into its own module

---
+++ src/BoundingBox.elm
@@ -0,0 +1,116 @@
+module BoundingBox (
+  BoundingBox,
+  boundingBox,
+  width,
+  height,
+  halfWidth,
+  halfHeight,
+  center,
+  subdivideNW,
+  subdivideNE,
+  subdivideSW,
+  subdivideSE,
+  intersectBoundingBoxes) where
+
+{-| AABB Bounding box implementation in Elm.
+
+# Bounding Boxes
+@docs boundingBox, intersectBoundingBoxes, width, height, halfWidth, halfHeight, center
+
+-}
+
+type alias Interval = {
+  low : Float,
+  high : Float
+}
+
+pointInInterval : Float -> Interval -> Bool
+pointInInterval point interval =
+  point < interval.high && point > interval.low
+
+intersectIntervals : Interval -> Interval -> Bool
+intersectIntervals interval1 interval2 =
+  pointInInterval interval1.low interval2
+
+--------
+type alias BoundingBox = {
+  horizontal : Interval,
+  vertical : Interval
+}
+
+{-| Function to determine if two bounding boxes intersect.
+-}
+intersectBoundingBoxes : BoundingBox -> BoundingBox -> Bool
+intersectBoundingBoxes box1 box2 =
+  intersectIntervals box1.horizontal box2.horizontal &&
+  intersectIntervals box1.vertical box2.vertical
+
+
+{-| Construct a bounding box.
+
+    boundingBox minX maxX minY maxY
+-}
+boundingBox : Float -> Float -> Float -> Float -> BoundingBox
+boundingBox minX maxX minY maxY =
+  BoundingBox (Interval minX maxX) (Interval minY maxY)
+
+{-| Get the width of a bounding box.
+-}
+width : BoundingBox -> Float
+width box =
+  box.horizontal.high - box.horizontal.low
+
+{-| Get the height of a bounding box.
+-}
+height : BoundingBox -> Float
+height box =
+  box.vertical.high - box.vertical.low
+
+{-| Get the half-width of a bounding box.
+-}
+halfWidth : BoundingBox -> Float
+halfWidth box =
+  width box / 2
+
+{-| Get the half-height of a bounding box.
+-}
+halfHeight : BoundingBox -> Float
+halfHeight box =
+  height box / 2
+
+{-| Get the center of a bounding box.
+-}
+center : BoundingBox -> {x : Float, y : Float}
+center box = {
+  x = box.horizontal.low + halfWidth box,
+  y = box.vertical.low + halfHeight box }
+
+
+subdivideNE : BoundingBox -> BoundingBox
+subdivideNE box =
+  let minX = box.horizontal.low + halfWidth box
+      minY = box.vertical.low + halfHeight box
+  in
+    boundingBox minX box.horizontal.high minY box.vertical.high
+
+subdivideNW : BoundingBox -> BoundingBox
+subdivideNW box =
+  let maxX = box.horizontal.high - halfWidth box
+      minY = box.vertical.low + halfHeight box
+  in
+    boundingBox box.horizontal.low maxX minY box.vertical.high
+
+subdivideSW : BoundingBox -> BoundingBox
+subdivideSW box =
+  let maxX = box.horizontal.high - halfWidth box
+      maxY = box.vertical.high - halfHeight box
+  in
+    boundingBox box.horizontal.low maxX box.vertical.low maxY
+
+subdivideSE : BoundingBox -> BoundingBox
+subdivideSE box =
+  let minX = box.horizontal.low + halfWidth box
+      maxY = box.vertical.high - halfHeight box
+  in
+    boundingBox minX box.horizontal.high box.vertical.low maxY
+
